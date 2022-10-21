956e9479208db89bda09091413a4f40ed24519b4
gabriel venegas aravena <gavenjr@hotmail.com>
2022-10-21 18:08:26 -0300

    Comienzo del proyecto

---
+++ proyecto_ubb/src/main/java/com/teamxploitdx/proyecto_ubb/ProyectoUbbApplication.java
@@ -0,0 +1,13 @@
+package com.teamxploitdx.proyecto_ubb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoUbbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoUbbApplication.class, args);
+	}
+
+}
---
+++ proyecto_ubb/src/test/java/com/teamxploitdx/proyecto_ubb/ProyectoUbbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teamxploitdx.proyecto_ubb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoUbbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
