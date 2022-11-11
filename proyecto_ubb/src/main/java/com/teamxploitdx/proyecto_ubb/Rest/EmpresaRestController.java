6bf3ad72e2728f4a5d10a7f454319bff59b43bc3
GavenJr <61298885+GavenJr@users.noreply.github.com>
2022-11-11 18:22:47 -0300

    Esqueleto del servicio HU_25

---
+++ proyecto_ubb/src/main/java/com/teamxploitdx/proyecto_ubb/Rest/EmpresaRestController.java
@@ -0,0 +1,10 @@
+package com.teamxploitdx.proyecto_ubb.Rest;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "empresas", produces = "application/json")
+public class EmpresaRestController {
+    
+}
