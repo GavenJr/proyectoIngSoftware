ba92b9c9eb08ea2a8a9b7b5eaafdc0ef74be090b
Francisca Belén Bello Gatica <francisca.bello1801@alumnos.ubiobio.cl>
2023-01-19 11:31:28 -0300

    correcciones HU_15

--- proyecto_ubb/src/main/java/com/teamxploitdx/proyecto_ubb/Repository/EmpresaRepository.java
+++ proyecto_ubb/src/main/java/com/teamxploitdx/proyecto_ubb/Repository/EmpresaRepository.java
@@ -1,4 +1,5 @@
 package com.teamxploitdx.proyecto_ubb.Repository;
+import java.util.List;
 import java.util.Optional;
 
 // imports de spring boot
@@ -7,9 +8,14 @@ import org.springframework.stereotype.Repository;
 
 // imports locales
 import com.teamxploitdx.proyecto_ubb.Model.Empresa;
+import com.teamxploitdx.proyecto_ubb.Model.Encuesta;
 
 @Repository     //indicamos que es un repositorio
 public interface EmpresaRepository extends JpaRepository<Empresa, Integer> {
     
     Optional<Empresa> findEmpresaByNombre(String nombre);   // Buscamos la empresa que nos interesa
+
+    static List<Encuesta> findAllByCategoria(String categoria) {
+        return null;
+    }
 }
