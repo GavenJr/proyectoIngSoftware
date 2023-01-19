ac07e804bea910ef9d7e8b84bc47d1ededc62fbc
GavenJr <61298885+GavenJr@users.noreply.github.com>
2023-01-19 16:07:28 -0300

    Fix Pull
    recuerden hacer pull request antes de hacer PUSH para evitar borrar progreso de master!
    Fue un fix diminuto, asi que queda aprobado!
    pero en proyectos reales, se habria rechazado para recibir uno corregido!

--- proyecto_ubb/src/main/java/com/teamxploitdx/proyecto_ubb/Repository/PreguntaRepository.java
+++ proyecto_ubb/src/main/java/com/teamxploitdx/proyecto_ubb/Repository/PreguntaRepository.java
@@ -1,16 +1,15 @@
-package com.teamxploitdx.proyecto_ubb.Repository;
-
-import java.util.List;
-import java.util.Optional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.teamxploitdx.proyecto_ubb.Model.Encuesta;
 import com.teamxploitdx.proyecto_ubb.Model.Pregunta;
 
 @Repository
 public interface PreguntaRepository extends JpaRepository <Pregunta, Integer> {
 
+	Optional <Pregunta> findPreguntaById(Integer id);
+	List <Pregunta> findPreguntasByEncuesta(Encuesta encuesta);
+	List<Pregunta> findAllPreguntaByEncuestaId(int id_encuesta);
 	List<Pregunta> findAllPreguntaByEncuestaId(int id_encuesta);
 
 
